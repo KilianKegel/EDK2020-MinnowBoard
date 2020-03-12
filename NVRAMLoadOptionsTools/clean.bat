0eb44b1237aa8cdd77bbc5a591a6f24d1d7a91bb
KG <kilian_kegel@hotmail.com>
2020-03-12 07:46:05 +0100

    improve command line tools -> LoadOption.efi, one fits all

--- NVRAMLoadOptionsTools/clean.bat
+++ NVRAMLoadOptionsTools/clean.bat
@@ -3,5 +3,13 @@ echo clean up...
 if exist x64 del /s x64\*.pdb
 if exist x64 del /s x64\*.map
 if exist x64 del /s x64\*.ilk
-for /D %%d in (argcv welcome*) do if exist %%d\x64 rd /s /q %%d\x64
+rem for /D %%d in (LstLdOpt SetLdOpt) do if exist %%d\x64 rd /s /q %%d\x64
+
+for /F "tokens=1-4 delims==," %%a in ('type *.sln ^| find ".vcxproj"') do (
+    for  %%o in (%%c) do (
+        echo %%~dpo
+        if exist %%~dpo\x64 rd /s /q %%~dpo\x64
+    )
+)
+rem pause
 
